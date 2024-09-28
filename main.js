a095bcf4d7dd38d85e1879a53a4bf1ea67d9b29f
HenriqueFLeao <henriquefleao@gmail.com>
2024-09-27 23:30:57 -0300

    Atualização de JS, com a adição da funcionalidade de copiar e mudar a imagemp para uma verificação visual do usuario

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
 const botaoCopy = document.querySelectorAll('.button_copy');
 
+// Adiciona um listener de clique a cada botão
+botaoCopy.forEach((botao) => {
+    botao.addEventListener('click', (event) => {
+        // Previne que o botão dentro do <a> execute a navegação do link
+        event.preventDefault();
+
+        //Pega o elemento <button> de casse .link relacionado ao botão de copiar
+        const linkElemento = botao.previousElementSibling
+
+        // Pega o valor do onclick que contém o link
+        const hrefValor = linkElemento.getAttribute('onclick');
+
+        //Extrai a URL do comando 'changeLocation'
+        const link = hrefValor.match(/'([^']+)'/)[1];
+
+        //Utiliza a API do Clipboard para copiar o link 
+        navigator.clipboard.writeText(link)
+
+        .then(()=>{
+            //Confirmação visual de que o link foi copiado 
+            const img = botao.querySelector('.img_copy');
+
+            //Armazena a imagem original
+            const imgOriginal = img.src;
+
+            //Muda a imagem para uma de "confimado"
+            img.src = './Assets/img/copiado.png';
+
+            //Volta à imagem original após 2 segundos
+            setTimeout(()=>{
+                img.src = imgOriginal;
+            },2000); //2000 milissegundos = 2 segundos
+        })
+
+    });
+});
+
 
 // Seleciona todos os elementos com a classe 'botao'
 const botoes = document.querySelectorAll('.botao');
@@ -28,4 +65,3 @@ links.forEach(link => {
 function changeLocation(url) {
   window.location.href = url; // Muda a URL da aba atual
 }
-
